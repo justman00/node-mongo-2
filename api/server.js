3ccdb7725904572c0988d5145c7b924ffdbb89a4
Vladimir Turcan <vladimir.turcan@sumup.com>
2021-03-04 18:17:44 +0200

    init commit

---
+++ api/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const server = express();
+
+server.use(express.json());
+
+server.get('/', (req, res) => {
+  res.send(`
+    <h2>Lambda Shelter API</h>
+    <p>Welcome to the Lambda Shelter API</p>
+  `);
+});
+
+module.exports = server;
